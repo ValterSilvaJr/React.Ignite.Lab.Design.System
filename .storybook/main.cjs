e95cef1cd95f6c789d2781b7fa7f803a36356a6b
Valter Silva <valtersilvajr@live.com>
2022-10-12 21:07:14 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION'){
+      config.base = '/React.Ignite.Lab.Design.System/'
+    }
+    
+    return config
   }
 }
